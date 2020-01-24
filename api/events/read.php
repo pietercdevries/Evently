33462959d1c7fa62240ab23c167f93d9ef190e1b
Pieter de Vries <pieterandcamryn@gmail.com>
2020-01-23 17:53:18 -0800

    Fix

--- api/events/read.php
+++ api/events/read.php
@@ -51,7 +51,7 @@ if($num>0)
             "evenTitle" => html_entity_decode($evenTitle),
             "eventTime" => html_entity_decode($eventTime),
             "eventDate" => html_entity_decode($eventDate),
-            "eventDescription" => html_entity_decode($eventDescription),
+            "eventDescription" => $eventDescription,
             "eventDistance" => html_entity_decode($eventDistance),
             "eventCategories" => html_entity_decode($eventCategories),
             "eventLikeCounter" => $eventLikeCounter,
